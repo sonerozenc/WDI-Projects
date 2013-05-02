9c1c83c650a40281e3811bb58214982c1e40eb8f
Soner Ozenc <sonerozenc@gmail.com>
2013-05-02 16:36:17 +0100

    Fix?

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -38,7 +38,8 @@ class JobsController < ApplicationController
       response = RestClient.post("https://api.sketchfab.com/v1/models", {
         token: "8765434590494ef890281bfb7c718b18",
         # fileModel: FileUploadIO.new('/Users/sonerozenc/Downloads/Up_Spool_Clip_v3-0.STL', "application/octet-stream"),
-        fileModel: FileUploadIO.new(@job.cad_file.url(:original, timestamp: false), "application/octet-stream"),
+        # fileModel: FileUploadIO.new(@job.cad_file.url(:original, timestamp: false), "application/octet-stream"),
+        fileModel: @job.cad_file.url(:original, timestamp: false),
         title: "Clip file",
         description: "This is my new clip file that I downloaded from Sketchfab",
         tags: "clip",
