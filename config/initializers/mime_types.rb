31f430fc8f1df47fb92c365ea80aef3508529697
Soner Ozenc <sonerozenc@gmail.com>
2013-05-02 16:05:36 +0100

    Added heroku fix

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,7 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+if Rack::Utils.respond_to?("key_space_limit=")
+  Rack::Utils.key_space_limit = 262144
+end
